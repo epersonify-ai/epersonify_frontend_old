08fc1bef4b2919deb49a97a733c07f52c09465c2
Kisara Kodithuwakku <kisarakd@gmail.com>
2024-09-05 19:26:01 +0530

    fixed scroll issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -55,7 +55,7 @@ export default function Layout({
     <div
       className={cn(
         "rounded-md flex flex-col md:flex-row bg-gray-100 dark:bg-neutral-800 w-full flex-1  mx-auto border border-neutral-200 dark:border-neutral-700 overflow-hidden",
-        "h-screen" // for your use case, use `h-screen` instead of `h-[60vh]`
+        "min-h-screen" // for your use case, use `h-screen` instead of `h-[60vh]`
       )}
     >
       <AuthModal />
